cc2018a6a39806c9163a3e8f31baae33dfea8abd
Randell Dawson <rdawson@onepathtech.com>
2020-12-30 09:50:40 -0700

    feat: annonate curriculum files

--- tools/crowdin/actions/annotate-curriculum-files/index.js
+++ tools/crowdin/actions/annotate-curriculum-files/index.js
@@ -1,16 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 const readDirP = require('readdirp');
+const {
+  annotate
+} = require('../../../formatter/translation-annotation/annotate');
 
 const ignoredFilesRE = /(part-\d\d\d\.md$)|(^\d\d-certificates)/i;
-const challengeDir = __dirname + '/../../../../curriculum/challenges/english';
+const challengeDir = path.join(
+  __dirname,
+  '/../../../../curriculum/challenges/english/04-data-visualization'
+);
 
-const displayCurriculumFiles = file => {
-  const match = file.path.match(ignoredFilesRE);
-  if (!match) {
-    console.log(file.path);
+const annotateCurriculumFiles = async file => {
+  try {
+    const match = file.path.match(ignoredFilesRE);
+    if (!match) {
+      const challengeFilePath = challengeDir + '/' + file.path;
+      const annotatedContent = await annotate(challengeFilePath);
+      fs.writeFileSync(challengeFilePath, annotatedContent);
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
 
 readDirP(challengeDir, { fileFilter: ['*.md'], type: 'files' }).on(
   'data',
-  displayCurriculumFiles
+  annotateCurriculumFiles
 );
